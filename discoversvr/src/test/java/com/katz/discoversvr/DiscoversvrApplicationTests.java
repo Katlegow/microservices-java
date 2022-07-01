9bb17584b1e1fdb7ca9228398905a04793614b01
Katlegow <mosweukatlego.km@gmail.com>
2022-07-01 17:59:25 +0200

    Added service discover service

---
+++ discoversvr/src/test/java/com/katz/discoversvr/DiscoversvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katz.discoversvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoversvrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
